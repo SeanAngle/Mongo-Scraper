fbd99600df27dfe1b2e1a1d2b07e7002342c860b
Sean Angle <seanangle@me.com>
2018-09-19 01:31:44 -0500

    Added a comments.handlebars file to display the comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ var cheerio = require("cheerio");
 
 module.exports = function (app) {
     app.get("/scrape", function(req, res) {
-        request("https://www.cnn.com/", function (error, response, html) {
+        request("https://www.yahoo.com/news/", function (error, response, html) {
             if(error){
                 console.log(error);
                 return res.json(error);
@@ -17,12 +17,11 @@ module.exports = function (app) {
                 // Add the text and href of every link, and save them as properties of the result object
                 result.title = $(element)
                   .text();
-                result.link = "https://www.cnn.com/" + $(element)
+                result.link = "https://www.yahoo.com" + $(element)
                   .attr("href");
-                result.id = $(element).reactid;
 
                   console.log("results: " + JSON.stringify(result));
-                // Create a new Article using the result object built from scraping
+                // Create a new Article using the `result` object built from scraping
                 db.Article.create(result)
                   .then(function(dbArticle) {
                     // View the added result in the console
@@ -38,35 +37,51 @@ module.exports = function (app) {
         })
     });
 
+    // Save an article
     app.post("/save/:id", function(req, res) {
-        db.Note.create(req.body)
-        .then(function(dbNote){
-        return db.Article.findOneAndUpdate(
+        db.Article.findOneAndUpdate(
             {_id: req.params.id},
             { saved: true },
-            );
-        })
-        .then(function(dbArticle){
-        res.json(dbArticle);
+        ).then(function(dbArticle){
+            res.json(dbArticle);
         })
         .catch(function(err) {
         res.json(err);
         })
     })
 
+
+    // Remove Article from Saved list
     app.post("/remove/:id", function(req, res) {
-        db.Note.create(req.body)
-        .then(function(dbNote){
-        return db.Article.findOneAndUpdate(
-            {id: req.params.id},
-            { saved: true },
-            );
-        })
-        .then(function(dbArticle){
-        res.json(dbArticle);
+        db.Article.findOneAndUpdate(
+            {_id: req.params.id},
+            { saved: false },
+        ).then(function(dbArticle){
+            res.redirect("/saved");
         })
         .catch(function(err) {
-        res.json(err);
+            res.json(err);
         })
     })
+
+    app.post("/articles/:id", function(req, res) {
+        db.Comment.create(req.body)
+          .then(function(dbComment){
+            return db.Article.findOneAndUpdate(
+                {_id: req.params.id},
+                { note: dbNote._id },
+                { new: true }
+              );
+          })
+          .then(function(dbArticle){
+            res.json(dbArticle);
+          })
+          .catch(function(err) {
+            res.json(err);
+          })
+      });
+
+
+
+
 };
